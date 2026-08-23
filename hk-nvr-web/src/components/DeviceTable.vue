774f21bdddce7e51475f14fe104883b76fadc1b6
Amentcity <1954869697@qq.com>
2026-08-23 23:35:06 +0800

    feat: complete enterprise nvr admin console

---
+++ hk-nvr-web/src/components/DeviceTable.vue
@@ -0,0 +1,10 @@
+<template>
+<el-table :data="devices">
+<el-table-column prop="name" label="设备名称"/>
+<el-table-column prop="ip" label="IP"/>
+<el-table-column prop="status" label="状态"/>
+</el-table>
+</template>
+<script setup lang="ts">
+defineProps<{devices:any[]}>()
+</script>
