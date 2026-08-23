528a689f9ae4207370534da3f4d1c55b3a6ddc38
Amentcity <1954869697@qq.com>
2026-08-23 23:37:54 +0800

    feat: complete enterprise nvr admin console

---
+++ hk-nvr-web/src/views/OperationLog.vue
@@ -0,0 +1,9 @@
+<template>
+  <LogTable :logs="logs" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import LogTable from '@/components/LogTable.vue'
+const logs = ref([])
+</script>
