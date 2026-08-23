6139eb8da79414475ff73593b5cc4ef614bd4640
Amentcity <1954869697@qq.com>
2026-08-23 20:00:26 +0800

    feat: add nvr web router layout and login

---
+++ hk-nvr-web/src/layout/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-container class="layout">
+    <el-header>Enterprise NVR</el-header>
+    <el-container>
+      <el-aside width="220px">
+        <el-menu router>
+          <el-menu-item index="/monitor">实时监控</el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style scoped>
+.layout{height:100vh}
+.el-header{background:#001529;color:white;display:flex;align-items:center;font-size:22px}
+</style>
