e3df693dd462e3bc64f9a4049b84c0ad1c863667
Amentcity <1954869697@qq.com>
2026-08-23 23:44:11 +0800

    feat: enhance enterprise nvr backend architecture

---
+++ src/main/java/com/amentcity/nvr/stream/vo/StreamInfo.java
@@ -0,0 +1,10 @@
+package com.amentcity.nvr.stream.vo;
+
+import lombok.Data;
+
+@Data
+public class StreamInfo {
+    private String streamId;
+    private String protocol;
+    private String url;
+}
