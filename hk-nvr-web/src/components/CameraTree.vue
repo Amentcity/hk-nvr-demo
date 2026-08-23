500cf68a7bcd1d945a924154fe68e6111ddb714f
Amentcity <1954869697@qq.com>
2026-08-23 20:01:38 +0800

    feat: add nvr realtime video wall and webrtc player

---
+++ hk-nvr-web/src/components/CameraTree.vue
@@ -0,0 +1,9 @@
+<template>
+  <el-tree :data="cameras" node-key="id" @node-click="select" />
+</template>
+
+<script setup lang="ts">
+defineProps<{cameras:any[]}>()
+const emit=defineEmits(['open'])
+function select(camera:any){emit('open',camera)}
+</script>
