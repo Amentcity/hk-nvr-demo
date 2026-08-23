9ae09ed4fbe795e9f92c1e533220a35db3b00138
Amentcity <1954869697@qq.com>
2026-08-23 20:07:07 +0800

    feat: complete realtime monitoring page

--- hk-nvr-web/src/components/LayoutSelector.vue
+++ hk-nvr-web/src/components/LayoutSelector.vue
@@ -1,15 +1,19 @@
 <template>
-  <el-radio-group v-model="layout" @change="change">
-    <el-radio-button :label="1">1分屏</el-radio-button>
-    <el-radio-button :label="4">4分屏</el-radio-button>
-    <el-radio-button :label="9">9分屏</el-radio-button>
-    <el-radio-button :label="16">16分屏</el-radio-button>
+  <el-radio-group v-model="layout" @change="changeLayout">
+    <el-radio-button :label="1">1画面</el-radio-button>
+    <el-radio-button :label="4">4画面</el-radio-button>
+    <el-radio-button :label="9">9画面</el-radio-button>
+    <el-radio-button :label="16">16画面</el-radio-button>
   </el-radio-group>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-const layout=ref(4)
-const emit=defineEmits(['change'])
-function change(){emit('change',layout.value)}
+
+const layout = ref(4)
+const emit = defineEmits(['change'])
+
+function changeLayout(){
+  emit('change', layout.value)
+}
 </script>
