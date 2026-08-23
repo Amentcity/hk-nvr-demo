3a7942cae194b63ef930a25e02e96f52b9ac9258
Amentcity <1954869697@qq.com>
2026-08-23 23:37:50 +0800

    feat: complete enterprise nvr admin console

---
+++ hk-nvr-web/src/views/RoleManage.vue
@@ -0,0 +1,9 @@
+<template>
+  <PermissionTree :data="permissions" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import PermissionTree from '@/components/PermissionTree.vue'
+const permissions = ref([])
+</script>
