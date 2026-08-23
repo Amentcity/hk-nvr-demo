7c89d29b99de1f717d66492e304b89180eaa9591
Amentcity <1954869697@qq.com>
2026-08-23 23:37:44 +0800

    feat: complete enterprise nvr admin console

---
+++ hk-nvr-web/src/views/UserManage.vue
@@ -0,0 +1,11 @@
+<template>
+  <el-table :data="users">
+    <el-table-column prop="username" label="用户名" />
+    <el-table-column prop="role" label="角色" />
+  </el-table>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const users = ref([])
+</script>
