a273c261bf89b0392a238e28c559140edf5c4a42
Amentcity <1954869697@qq.com>
2026-08-23 20:07:59 +0800

    feat: add nvr playback and timeline module

---
+++ hk-nvr-web/src/components/PlaybackWall.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wall">
+    <PlaybackPlayer v-for="item in streams" :key="item.taskId" :url="item.hlsUrl" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import PlaybackPlayer from './PlaybackPlayer.vue'
+defineProps<{streams:any[]}>()
+</script>
+
+<style scoped>
+.wall{display:grid;grid-template-columns:repeat(2,1fr);height:100%;gap:5px;background:#000}
+</style>
