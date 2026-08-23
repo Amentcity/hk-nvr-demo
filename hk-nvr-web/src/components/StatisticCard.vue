6349a3e4b6d69434ad6e56dff425910941b53575
Amentcity <1954869697@qq.com>
2026-08-23 20:17:50 +0800

    feat: add enterprise nvr admin console

---
+++ hk-nvr-web/src/components/StatisticCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-card>
+    <div>{{ title }}</div>
+    <h2>{{ value }}</h2>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+defineProps<{title:string,value:string|number}>()
+</script>
