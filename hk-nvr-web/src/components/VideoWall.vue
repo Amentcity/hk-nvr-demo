f539d8c06746574b28211e79406f72bfd2e1a78b
Amentcity <1954869697@qq.com>
2026-08-23 20:01:35 +0800

    feat: add nvr realtime video wall and webrtc player

---
+++ hk-nvr-web/src/components/VideoWall.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wall">
+    <WebRTCPlayer v-for="item in streams" :key="item.id" :url="item.url" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import WebRTCPlayer from './WebRTCPlayer.vue'
+defineProps<{streams:any[]}>()
+</script>
+
+<style scoped>
+.wall{display:grid;grid-template-columns:repeat(2,1fr);height:100%;gap:4px;background:#000}
+</style>
