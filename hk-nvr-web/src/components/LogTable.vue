1815dc7dcbca3fd691dd6df58d5afd8920f92ef6
Amentcity <1954869697@qq.com>
2026-08-23 23:35:18 +0800

    feat: complete enterprise nvr admin console

---
+++ hk-nvr-web/src/components/LogTable.vue
@@ -0,0 +1,10 @@
+<template>
+<el-table :data="logs">
+<el-table-column prop="username" label="用户"/>
+<el-table-column prop="operation" label="操作"/>
+<el-table-column prop="createTime" label="时间"/>
+</el-table>
+</template>
+<script setup lang="ts">
+defineProps<{logs:any[]}>()
+</script>
