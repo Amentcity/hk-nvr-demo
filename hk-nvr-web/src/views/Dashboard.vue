7549982aa6583841b36d344350c011a294edd03c
Amentcity <1954869697@qq.com>
2026-08-23 20:17:59 +0800

    feat: add enterprise nvr admin console

---
+++ hk-nvr-web/src/views/Dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="6"><StatisticCard title="NVR设备" value="12"/></el-col>
+    <el-col :span="6"><StatisticCard title="摄像头" value="256"/></el-col>
+    <el-col :span="6"><StatisticCard title="在线流" value="128"/></el-col>
+    <el-col :span="6"><StatisticCard title="告警" value="32"/></el-col>
+  </el-row>
+</template>
+
+<script setup lang="ts">
+import StatisticCard from '@/components/StatisticCard.vue'
+</script>
