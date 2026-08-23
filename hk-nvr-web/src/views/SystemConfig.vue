c521ac9cc682c3bc52914946b5f0988821cabaf5
Amentcity <1954869697@qq.com>
2026-08-23 23:37:58 +0800

    feat: complete enterprise nvr admin console

---
+++ hk-nvr-web/src/views/SystemConfig.vue
@@ -0,0 +1,9 @@
+<template>
+  <el-card>
+    <h3>NVR系统配置</h3>
+    <el-form>
+      <el-form-item label="服务地址"><el-input /></el-form-item>
+      <el-form-item label="WebRTC服务器"><el-input /></el-form-item>
+    </el-form>
+  </el-card>
+</template>
