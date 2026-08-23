6a9a76607fe21987d776471104d2a8df9efce587
Amentcity <1954869697@qq.com>
2026-08-23 23:37:40 +0800

    feat: complete enterprise nvr admin console

---
+++ hk-nvr-web/src/views/CameraManage.vue
@@ -0,0 +1,9 @@
+<template>
+  <CameraTable :cameras="cameras" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import CameraTable from '@/components/CameraTable.vue'
+const cameras = ref([])
+</script>
