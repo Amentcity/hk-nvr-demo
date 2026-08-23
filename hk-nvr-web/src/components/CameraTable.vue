f3b92809b8f2ffcd3a401e2f61df823caa393cf4
Amentcity <1954869697@qq.com>
2026-08-23 23:35:10 +0800

    feat: complete enterprise nvr admin console

---
+++ hk-nvr-web/src/components/CameraTable.vue
@@ -0,0 +1,10 @@
+<template>
+<el-table :data="cameras">
+<el-table-column prop="name" label="摄像头"/>
+<el-table-column prop="channel" label="通道"/>
+<el-table-column prop="streamUrl" label="流地址"/>
+</el-table>
+</template>
+<script setup lang="ts">
+defineProps<{cameras:any[]}>()
+</script>
