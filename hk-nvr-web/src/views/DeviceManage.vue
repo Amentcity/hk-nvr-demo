71d62494c0a14a8203c3db787af746ba17e2d804
Amentcity <1954869697@qq.com>
2026-08-23 23:37:36 +0800

    feat: complete enterprise nvr admin console

---
+++ hk-nvr-web/src/views/DeviceManage.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-card>
+    <el-button type="primary">新增NVR设备</el-button>
+    <DeviceTable :devices="devices" />
+  </el-card>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import DeviceTable from '@/components/DeviceTable.vue'
+const devices = ref([])
+</script>
